b9d7fc4e2521b3673e47eddef1fcc5edf3449684
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-11-01 15:35:04 +0100

    added even listeners to all html components

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     // reference all components/buttons 
-    const numbers = document.querySelectorAll(".number");
-    numbers.forEach((number) => {
+    const numberBtns = document.querySelectorAll(".number");
+    numberBtns.forEach((number) => {
         number.addEventListener("click", () => {
-            alert("hi");
+            alert("im a number!");
         })
     })
 
+    const operatorBtns = document.querySelectorAll(".operator");
+    operatorBtns.forEach((operator) => {
+        operator.addEventListener("click", () => {
+            alert("im an operator");
+        })
+    })
+
+    const dotBtn = document.querySelector("#dot");
+    dotBtn.addEventListener("click", () => {
+        alert(".");
+    })
+
+    const clearBtn = document.querySelector("#clear");
+    clearBtn.addEventListener("click", () => {
+        alert("i clear things!");
+    })
+
+    const equalBtn = document.querySelector("#equal");
+    equalBtn.addEventListener("click", () => {
+        alert("i calculate things!");
+    })
 
     let operator = "";
     let firstOperand = "";
